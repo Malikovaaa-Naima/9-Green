db7e43dfc45f963fcb4351ee5c266a241373e05a
Malikovaaa-Naima <naimamalikova880@gmail.com>
2025-02-27 08:51:48 +0500

    пол

--- 9-Green/Yangi__py✨/lesson_16_try.py
+++ 9-Green/Yangi__py✨/lesson_16_try.py
@@ -23,9 +23,28 @@
 # except:
 #     print("Xatolik!")
 
+""" 1 mashq """
+def cal(son1, son2,icon):
+    """ hozir men calcu yasayman."""
 
-
-
+    try:
+        if icon == '+':
+            return son1 + son2
+        elif icon == '-':
+            return son1 - son2
+        elif icon == '*':
+            return son1 * son2
+        elif icon == '/':
+            return son1 / son2
+        else:
+            return "Error! Bunday bir operator yo'q."
+    except ZeroDivisionError:
+        return "Error! 0 ga bolib bolmaydi."
+    except TypeError:
+        return "Error!"
+    except KeyError as e:
+        return f"Error! {str(e)}"
+    
 
 
 
